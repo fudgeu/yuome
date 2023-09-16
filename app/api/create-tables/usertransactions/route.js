463a98e72c2bc289b2c71ae65afd6af59d225c1e
Kobe <kobechua14@gmail.com>
2023-09-16 19:43:56 -0400

    create users and transactions

--- app/api/create-tables/usertransactions/route.js
+++ app/api/create-tables/usertransactions/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
   try {
     const result =
-      await sql`CREATE TABLE Users ( name varchar(255), phone_number varchar(10) );`;
+      await sql`CREATE TABLE UserTransactions ( pn_to varchar(10), fk_id int, pn_from varchar(10) );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
