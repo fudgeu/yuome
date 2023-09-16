04509f6e7f7390bb6b487524736dc2f79df1b444
Kobe <kobechua14@gmail.com>
2023-09-16 16:10:06 -0400

    still working on it

--- app/api/create-tables/transactions/route.js
+++ app/api/create-tables/transactions/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export async function GET(request) {
   try {
     const result =
-      await sql`CREATE OR REPLACE TABLE Transactions ( owed_to varchar(255), owed_by varchar(255), amount float(10) );`;
+      await sql`CREATE OR REPLACE TABLE Transactions ( owed_to varchar(255), owed_by varchar(255), amount float(10), r_date date );`;
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
