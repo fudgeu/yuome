3f84da4025ba66861d797a9e820d666b8e83230d
Patrick <patrickzambonie@gmail.com>
2023-09-17 01:09:31 -0400

    Added functionality to all tabs section

--- app/api/get-user-info/route.js
+++ app/api/get-user-info/route.js
@@ -9,8 +9,6 @@ export async function POST(request) {
  
   try {
     
-    data.pn_to = data.phone_number.split(" ").join("")
-
     if (data.phone_number.startsWith('+1')){
       data.phone_number = data.phone_number.substring(2,12)
     }
@@ -19,6 +17,7 @@ export async function POST(request) {
     const res  = await sql`SELECT * FROM transactions as t, usertransactions as u WHERE t.id=u.fk_id and (u.pn_to=${data.phone_number} or u.pn_from=${data.phone_number}) ORDER BY t.r_date`;
     return NextResponse.json( res.rows , { status: 200 });
   } catch (error) {
+    console.log(error)
     return NextResponse.json({ error }, { status: 500 });
   }
 
--- app/components/all-tabs/all-tabs.jsx
+++ app/components/all-tabs/all-tabs.jsx
@@ -1,7 +1,7 @@
 'use client'
 import styles from './all-tabs.module.css'
 import UserTab from '../user-tab/user-tab'
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useUser } from '@clerk/nextjs'
 
 const testTab = [
@@ -30,27 +30,78 @@ const testTab = [
 
 export default function AllTabs() {
 
+  const [tabs, setTabs] = useState({});
   const { isSignedIn, user } = useUser();
 
   useEffect(() => {
+    if (user == null) return
     fetch(
       "/api/get-user-info",
       {
         method: "POST",
         body: JSON.stringify({
-          phone_number: user?.primaryPhoneNumber.phoneNumber
+          phone_number: user.primaryPhoneNumber.phoneNumber
         }),
         headers: {
           "content-type": "application/json",
         },
       }).then((resp) => {
-        console.log(resp)
+        return resp.json()
+      }).then((json) => {
+        console.log(json)
+
+        // Fix user phone number
+        let curUser = user.primaryPhoneNumber.phoneNumber
+        if (curUser.startsWith("+1")) {
+          curUser = curUser.substring(2, 12)
+        }
+
+        // Sort data into each user
+        const sortedData = {}
+        json.forEach((transaction) => {
+          const parsedTransaction = {}
+          parsedTransaction.amount = transaction.amount
+          parsedTransaction.created = transaction.r_date
+          parsedTransaction.id = transaction.id
+          if (curUser === transaction.pn_from) {
+            if (transaction.type === "req") {
+              parsedTransaction.type = "requestTo"
+            } else if (transaction.type === 'pay') {
+              parsedTransaction.type = "paymentTo"
+            }
+            if (sortedData[transaction.pn_to] == null) sortedData[transaction.pn_to] = []
+            sortedData[transaction.pn_to].push(parsedTransaction)
+          } else if (curUser === transaction.pn_to) {
+            if (transaction.type === "req") {
+              sortedData[transaction.pn_from] = {
+                type: "requestFrom",
+              }
+            } else if (transaction.type === 'pay') {
+              sortedData[transaction.pn_from] = {
+                type: "paymentFrom"
+              }
+            }
+            if (sortedData[transaction.pn_from] == null) sortedData[transaction.pn_from] = []
+            sortedData[transaction.pn_from].push(parsedTransaction)
+          }
+        })
+        setTabs(sortedData)
       })
-  }, [user?.primaryPhoneNumber])
+  }, [user])
 
   return (
     <div className={styles.tabList}>
-      <UserTab user="UserX" tab={testTab} />
+      {Object.keys(tabs).map((user) => {
+        console.log(`mapping: ${user}`)
+        console.log(tabs[user])
+        return (<UserTab key={user} user={user} tab={tabs[user]} />)
+      })}
+    </div>
+  )
+}
+
+/*
+<UserTab user="UserX" tab={testTab} />
       <UserTab user="UserY" tab={testTab} />
       <UserTab user="UserZ" tab={testTab} />
       <UserTab user="UserW" tab={testTab} />
@@ -61,6 +112,4 @@ export default function AllTabs() {
       <UserTab user="UserYW" tab={testTab} />
       <UserTab user="UserYX" tab={testTab} />
       <UserTab user="UserZZ" tab={testTab} />
-    </div>
-  )
-}
+      */
